43aab662d201edacb4681b6b0af24524a7ca7d47
Raymond Cheng <ryscheng@cs.washington.edu>
2014-05-20 12:28:03 -0700

    Test successfully runs

--- tools/karma-cordova-launcher/index.js
+++ tools/karma-cordova-launcher/index.js
@@ -11,10 +11,10 @@ function runCordovaCmd(args) {
     cwd: CORDOVA_DIR
   }).progress(function(childProcess) {
     childProcess.stdout.on('data', function(data) {
-      console.log('[spawn] stdout: ' + data.toString());
+      console.log('[spawn] stdout: ' + data.toString().trim());
     });
     childProcess.stderr.on('data', function(data) {
-      console.error('[spawn] stderr: ' + data.toString());
+      console.error('[spawn] stderr: ' + data.toString().trim());
     });
   });
 }
@@ -27,6 +27,11 @@ var Cordova = function(id, emitter, args, logger, config) {
 
   console.log(self.settings);
 
+  emitter.on('exit', function(done){
+    console.log("!!!EXITING!!!"); 
+    done();
+  });
+
   var errorHandler = function(err) {
     self.log.error(err);
     emitter.emit('browser_process_failure', self);
@@ -34,36 +39,49 @@ var Cordova = function(id, emitter, args, logger, config) {
 
   this.start = function(url) {
     self.log.debug("Starting at " + url);
-    ncp(TEMPLATE_DIR, CORDOVA_DIR, function(err) {
-      if (err) {
-        self.log.error(err);
-        emitter.emit('browser_process_failure', self);
+    ncp(TEMPLATE_DIR, CORDOVA_DIR, function(ncp_err) {
+      if (ncp_err) {
+        errorHandler(ncp_err);
         return;
       }
-      
-      var platforms = self.settings.platforms;
-      var plugins = self.settings.plugins;
-      var promise = runCordovaCmd(['plugin', 'add'].concat(plugins)).fail(errorHandler);
-      
-      for (var i=0; i<platforms.length; i++) {
-        promise = promise.then(
-          runCordovaCmd.bind({}, ['platform', 'add', platforms[i]]),
-          runCordovaCmd.bind({}, ['platform', 'add', platforms[i]])
-        );
-      }
-      promise = promise.then(function(result) {
-        console.log('Done adding platforms');
-      }, function(err) {
-        console.log('Done adding platforms');
-      });
-      
-      promise = promise.then(runCordovaCmd.bind({}, ['build']), errorHandler);
-      promise.then(function() {
-        for (var i=0; i<platforms.length; i++) {
-          runCordovaCmd(['emulate', platforms[i]], errorHandler); 
+      fs.readFile(CORDOVA_DIR + "/www/index.html", function(read_err, read_data) {
+        if (read_err) {
+          errorHandler(read_err);
+          return;
         }
-      }, errorHandler);
+        var newUrl = url + "?id=" + id;
+        newUrl = newUrl.replace(/localhost/g, "10.0.2.2");
+        var toWrite = read_data.toString().replace(/NEWURL/g, newUrl);
+        fs.writeFile(CORDOVA_DIR + "/www/index.html", toWrite, function (write_err) {
+          if (write_err) {
+            errorHandler(write_err);
+            return;
+          }
+          var platforms = self.settings.platforms;
+          var plugins = self.settings.plugins;
+          var promise = runCordovaCmd(['plugin', 'add'].concat(plugins)).fail(errorHandler);
       
+          for (var i=0; i<platforms.length; i++) {
+            promise = promise.then(
+              runCordovaCmd.bind({}, ['platform', 'add', platforms[i]]),
+              runCordovaCmd.bind({}, ['platform', 'add', platforms[i]])
+            );
+          }
+          promise = promise.then(function(result) {
+            console.log('Done adding platforms');
+          }, function(err) {
+            console.log('Done adding platforms');
+          });
+      
+          promise = promise.then(runCordovaCmd.bind({}, ['build']), errorHandler);
+          promise.then(function() {
+            for (var i=0; i<platforms.length; i++) {
+              runCordovaCmd(['emulate', platforms[i]], errorHandler); 
+            }
+          }, errorHandler);
+
+        });
+      });
     });
   };
 
