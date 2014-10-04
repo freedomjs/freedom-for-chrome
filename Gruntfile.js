dbb71f91c14c59ae08bdbb1aeb4de7d29c8ffec6
Will Scott <willscott@gmail.com>
2014-10-04 12:10:52 -0400

    finalize integration, all tests pass locally

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,7 +107,7 @@ module.exports = function (grunt) {
             {path: freedomPrefix + '/providers', name: 'providers', include: false},
             {path: freedomPrefix + '/spec', name: 'spec', include: false}
           ],
-          keepBrowser: true
+          keepBrowser: false
         }
       }
     }
